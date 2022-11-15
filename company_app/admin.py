9eda53a54a503cd7b6ccc5a32dc40c82f7be8fdb
Vishnu VP <vishnuvvp007@gmail.com>
2022-11-15 21:17:14 +0530

    admin add

--- company_app/admin.py
+++ company_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+from django.apps import apps
 
-# Register your models here.
+apps = apps.get_app_config('company_app')
+
+for model_name, model in apps.models.items():
+    admin.site.register(model)
