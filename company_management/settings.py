75a25bb1fb89bc1bc9b37b01395ead3b9771b4bc
Vishnu VP <vishnuvvp007@gmail.com>
2022-11-20 14:46:07 +0530

    chanhnes

--- company_management/settings.py
+++ company_management/settings.py
@@ -109,27 +109,27 @@ WSGI_APPLICATION = 'company_management.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'malfati_user',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'malfati_db',
+#         'NAME': 'malfati_user',
 #         'USER': 'postgres',
-#         'PASSWORD': 'Malfati123Bhooma',
+#         'PASSWORD': 'postgres',
 #         'HOST': 'localhost',
 #         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'malfati_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Malfati123Bhooma',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
