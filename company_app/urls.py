2f198c8ab95c576b36ed7696b73023d5847791ca
Vishnu VP <vishnuvvp007@gmail.com>
2022-11-21 00:27:14 +0530

    file upload

--- company_app/urls.py
+++ company_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('file-upload', FileUploadBase64.as_view()),
+    path('file-upload-64', FileUploadBase64.as_view()),
+    path('file-upload', FileUpload.as_view()),
     path('service-request-send', ServicesRequestsCreate.as_view()),
     path('service-request-list', ServicesRequestsApproval.as_view()),
     path('my-service-request', MyServicesRequests.as_view()),
