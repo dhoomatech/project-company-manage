926700520557e59ec48a3695ad49abc24d5cac78
Vishnu VP <vishnuvvp007@gmail.com>
2023-01-11 03:16:59 +0530

    admin

--- test.py
+++ test.py
@@ -11,7 +11,8 @@ django.setup()
 
 from user_manage.models import LoginUser
 
-user_obj = LoginUser.objects.filter(email="sreejithsasidharanp@gmail.com").first()
+user_obj = LoginUser.objects.filter(email="vishnu@dhoomatech.com").first()
 user_obj.is_staff = True
 user_obj.is_superuser = True
 user_obj.save()
+print(user_obj.__dict__)
