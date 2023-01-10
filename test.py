5445ff37e5821d273085712d70b205c0e1d3773d
Vishnu VP <vishnuvvp007@gmail.com>
2023-01-11 03:11:43 +0530

    admin

--- test.py
+++ test.py
@@ -5,6 +5,13 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "company_management.settings")
 django.setup()
 
-from dtuser_auth.views import sms_twilio_send
+# from dtuser_auth.views import sms_twilio_send
 
-sms_twilio_send()
+# sms_twilio_send()
+
+from user_manage.models import LoginUser
+
+user_obj = LoginUser.objects.filter(email="sreejithsasidharanp@gmail.com").first()
+user_obj.is_staff = True
+user_obj.is_superuser = True
+user_obj.save()
