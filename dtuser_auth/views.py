c0aca2856c5959eb633d8853b1693d7f75697112
Vishnu VP <vishnuvvp007@gmail.com>
2023-01-13 00:44:45 +0530

    error

--- dtuser_auth/views.py
+++ dtuser_auth/views.py
@@ -15,12 +15,13 @@ def sms_twilio_send():
     # Find your Account SID and Auth Token in Account Info and set the environment variables.
     # See http://twil.io/secure
     account_sid = "AC710fde2a95d194182b74045bd8766186"
-    auth_token = "42e4a823b80372addb60a29267482b7d"
+    auth_token = "7ac33a5dffce0630da84b50f826f2e3b"
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         body='Hi there vishnu vp this message from dhoomatech.',
         from_='+16402237936',
         to='+919633752456'
+        # to='+918943788706'
     )
 
     print(message.sid)
