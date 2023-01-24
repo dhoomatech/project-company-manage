69aa669beaa1239bfab5c978a522e7276a5940fb
Vishnu VP <vishnuvvp007@gmail.com>
2023-01-24 07:25:25 +0530

    trial twilio

--- dtuser_auth/views.py
+++ dtuser_auth/views.py
@@ -15,11 +15,11 @@ def sms_twilio_send():
 
     # Find your Account SID and Auth Token in Account Info and set the environment variables.
     # See http://twil.io/secure
-    account_sid = "ACf5fee4714f7a25f1b15c419cc544e958"
-    auth_token = "c249c9dd51b396de064216e23a4f3e4b"
+    # account_sid = "ACf5fee4714f7a25f1b15c419cc544e958"
+    # auth_token = "c249c9dd51b396de064216e23a4f3e4b"
     try:
-        # client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-        client = Client(account_sid, auth_token)
+        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        # client = Client(account_sid, auth_token)
         message = client.messages.create(
             body='Hi there vishnu vp this message from dhoomatech.',
             from_='+13855263363',
