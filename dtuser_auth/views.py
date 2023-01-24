692dbe78efb9e1ca8086a4013e079fd390730d65
Vishnu VP <vishnuvvp007@gmail.com>
2023-01-24 07:17:21 +0530

    trial twilio

--- dtuser_auth/views.py
+++ dtuser_auth/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from django.conf import settings
+import traceback
 # Create your views here.
 
 # TWILIO_ACCOUNT_SID = 'AC48fe8f20b2328ba4076368794fe7c766'
@@ -14,16 +15,25 @@ def sms_twilio_send():
 
     # Find your Account SID and Auth Token in Account Info and set the environment variables.
     # See http://twil.io/secure
-    account_sid = "AC710fde2a95d194182b74045bd8766186"
-    auth_token = "7ac33a5dffce0630da84b50f826f2e3b"
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        body='Hi there vishnu vp this message from dhoomatech.',
-        from_='+18885083539',
-        to='+919633752456'
-        # to='+918943788706'
-    )
+    account_sid = "ACf5fee4714f7a25f1b15c419cc544e958"
+    auth_token = "c249c9dd51b396de064216e23a4f3e4b"
+    try:
+        # client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body='Hi there vishnu vp this message from dhoomatech.',
+            from_='+13855263363',
+            to='+919633752456'
+            # to='+918943788706'
+        )
+        print(message.sid)
 
-    print(message.sid)
+        # incoming_phone_number = client.incoming_phone_numbers.create(phone_number='9446458004')
+        # print(incoming_phone_number.sid)
+    
+    except:
+        traceback.print_exc()
+
+
 
     
