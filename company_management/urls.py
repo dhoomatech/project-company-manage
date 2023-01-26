7e39611dc932b3904a3f600377f615c869d1c91d
Vishnu VP <vishnuvvp007@gmail.com>
2023-01-26 23:46:43 +0530

    admin redirect

--- company_management/urls.py
+++ company_management/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from .view_func import redirect_view
 
 urlpatterns = [
+    path('', redirect_view),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('user/', include('user_manage.urls')),
