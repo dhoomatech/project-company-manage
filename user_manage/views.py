948555568b042011b745b065356b0557f5a73270
Dhooma Tech <116483685+dhoomatech@users.noreply.github.com>
2023-02-20 11:34:08 +0530

    Update views.py

--- user_manage/views.py
+++ user_manage/views.py
@@ -306,6 +306,9 @@ class CreateEmployee(APIView):
             if 'company_id' in post_data and post_data['company_id']:
             	user_obj = LoginUser.objects.filter(id=post_data['company_id']).first()
 
+            if not user_obj:
+            	return Response({"status":400,"message":"Not a valid company details."})
+
             company_obj = EmployeeDetails()
             company_obj.company = user_obj
             company_obj.code = post_data['code']
